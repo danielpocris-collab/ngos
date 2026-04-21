33e5624a30f353702ba926cfb4237f8d70b00a99
danielpocris-collab <danielpocris@gmail.com>
2026-04-21 04:16:07 +0300

    repo: remove tracked cargo build artifacts

--- .cargo-target-diagnosticsfPIVQM/debug/deps/ngos_game_compat_runtime-54c7770cf47fd807.d
+++
@@ -1,7 +0,0 @@
-C:\Users\pocri\OneDrive\Desktop\experiment\.cargo-target-diagnosticsfPIVQM\debug\deps\ngos_game_compat_runtime-54c7770cf47fd807.d: game-compat-runtime\src\lib.rs
-
-C:\Users\pocri\OneDrive\Desktop\experiment\.cargo-target-diagnosticsfPIVQM\debug\deps\libngos_game_compat_runtime-54c7770cf47fd807.rlib: game-compat-runtime\src\lib.rs
-
-C:\Users\pocri\OneDrive\Desktop\experiment\.cargo-target-diagnosticsfPIVQM\debug\deps\libngos_game_compat_runtime-54c7770cf47fd807.rmeta: game-compat-runtime\src\lib.rs
-
-game-compat-runtime\src\lib.rs:
--- .cargo-target-diagnosticsfPIVQM/debug/deps/ngos_user_abi-b56a1fc0dadb0b68.d
+++
@@ -1,8 +0,0 @@
-C:\Users\pocri\OneDrive\Desktop\experiment\.cargo-target-diagnosticsfPIVQM\debug\deps\ngos_user_abi-b56a1fc0dadb0b68.d: user-abi\src\lib.rs user-abi\src\bootstrap.rs
-
-C:\Users\pocri\OneDrive\Desktop\experiment\.cargo-target-diagnosticsfPIVQM\debug\deps\libngos_user_abi-b56a1fc0dadb0b68.rlib: user-abi\src\lib.rs user-abi\src\bootstrap.rs
-
-C:\Users\pocri\OneDrive\Desktop\experiment\.cargo-target-diagnosticsfPIVQM\debug\deps\libngos_user_abi-b56a1fc0dadb0b68.rmeta: user-abi\src\lib.rs user-abi\src\bootstrap.rs
-
-user-abi\src\lib.rs:
-user-abi\src\bootstrap.rs:
